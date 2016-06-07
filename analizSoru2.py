d2a24632a7436bda3156837eb3a875e4c627bff8
HabibeBegde <habibebegdee@gmail.com>
2016-06-07 16:36:14 +0300

    Update analizSoru2.py

--- analizSoru2.py
+++ analizSoru2.py
@@ -1,68 +1,37 @@
-dosya=open("tablo1.txt","r")
+import itertools
+dosya=open("tablo2.txt", "r")
+#headerlar baslik dizisi icerisinde
 baslik=dosya.readline().split(",")
-girdi=dosya.readlines(1)
+#veriler veri icerisinde. her satir bir dizi elemani
+veri=dosya.readlines(1)
 boyut=len(baslik)
-for i in girdi:
-     for k in i.split(","):
-         print k,
-print "\n"
 
-"""for j in range(0,boyut):
-    for i in girdi:
-        print i.split(",")[j].replace('\n',''), """
+for adet in range(1,boyut+1):
+    komb=list(itertools.combinations(range(0,boyut), adet))
+    maks=0
+    for eleman in komb:
+        curmaks=0
+        for satir in veri:
+            durum=1
+            for indis in eleman:
+                deger=satir.split(",")[indis].replace('\n','')
+                if(deger=='0'):
+                    durum=0
+            if durum==1:
+                curmaks=curmaks+1
 
-maxOne=0
-for j in range(0,boyut):
-    maxOne1=0
-    for i in girdi:
-        deger=i.split(",")[j].replace('\n','')
-        if(deger=="1"):
-            maxOne1=maxOne1+1
-    if(maxOne1>maxOne):
-        maxOne=maxOne1
-        indis=j
-print ("en fazla 1 olan sutun %s ve 1'lerin sayisi:%s" %(baslik[indis],maxOne))
-
-maxOne=0
-for k in range (0,boyut-1):
-    for j in range(k+1,boyut):
-      maxOne1=0
-      for i in girdi:
-         degerj=i.split(",")[j].replace('\n','')
-         degerk=i.split(",")[k].replace('\n','')
-        
-         if(degerj=="1" and degerk=="1"):
-            maxOne1=maxOne1+1
-      if(maxOne1>maxOne):
-        maxOne=maxOne1
-        indisj=j
-        indisk=k
-print ("en fazla 1 olan ikili sutunlar %s  %s ve 1'lerin sayisi:%s" %(baslik[indisj],baslik[indisk],maxOne))
+        if(curmaks>maks):
+            maks=curmaks
+            indisler=eleman
 
 
-maxOne=0
-for l in range (0,boyut-2):
-    for k in range (l+1,boyut-1):
-        for j in range(k+1,boyut):
-            maxOne1=0
-            for i in girdi:
-               degerj=i.split(",")[j].replace('\n','')
-               degerk=i.split(",")[k].replace('\n','')
-               degerl=i.split(",")[l].replace('\n','')
-        
-               if(degerj=="1" and degerk=="1" and degerl=="1"):
-                     maxOne1=maxOne1+1
-        if(maxOne1>maxOne):
-           maxOne=maxOne1
-           indisj=j
-           indisk=k
-           indisl=l
-print ("en fazla 1 olan uclu sutunlar %s %s %s ve 1'lerin sayisi:%s" %(baslik[indisl],baslik[indisk],baslik[indisj],maxOne))
+    urun=""
+    for i in indisler:
+        urun=urun+baslik[i].replace('\n','')
 
-    
-
-
-
-
-
-
+    print("------- %s karsilastirma ------"%adet)
+    if(maks==0):
+        print ("%s adet urun icin maks bulunamadi"%adet)
+    else:
+        print ("%s urunleri %s kere satildi"%(urun,maks))
+    print("")
