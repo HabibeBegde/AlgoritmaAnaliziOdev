c912c134c4fb910fb9c4460ed869207bac564edd
HabibeBegde <habibebegdee@gmail.com>
2016-06-07 16:47:25 +0300

    Update analizSoru2.py

--- analizSoru2.py
+++ analizSoru2.py
@@ -1,37 +1,36 @@
 import itertools
-dosya=open("tablo2.txt", "r")
-#headerlar baslik dizisi icerisinde
+
+dosya=open("tablo1.txt","r")
 baslik=dosya.readline().split(",")
-#veriler veri icerisinde. her satir bir dizi elemani
-veri=dosya.readlines(1)
+girdi=dosya.readlines(1)
 boyut=len(baslik)
-
-for adet in range(1,boyut+1):
-    komb=list(itertools.combinations(range(0,boyut), adet))
-    maks=0
-    for eleman in komb:
-        curmaks=0
-        for satir in veri:
-            durum=1
-            for indis in eleman:
-                deger=satir.split(",")[indis].replace('\n','')
-                if(deger=='0'):
-                    durum=0
-            if durum==1:
-                curmaks=curmaks+1
-
-        if(curmaks>maks):
-            maks=curmaks
-            indisler=eleman
+for i in girdi:
+    for k in i.split(","):
+         print k,
+print "\n"
 
 
-    urun=""
-    for i in indisler:
-        urun=urun+baslik[i].replace('\n','')
-
-    print("------- %s karsilastirma ------"%adet)
-    if(maks==0):
-        print ("%s adet urun icin maks bulunamadi"%adet)
-    else:
-        print ("%s urunleri %s kere satildi"%(urun,maks))
-    print("")
+for sutun in range(1,boyut+1):
+    kombinasyon=list(itertools.combinations(range(0,boyut), sutun))
+    maxOne=0
+    for komb in kombinasyon :
+         maks=0
+         for i in girdi:
+               test=1
+               for k in komb:
+                     deger=i.split(",")[k].replace('\n','')
+                     if(deger=='0'):
+                          test=0
+               if test==1:
+                     maks=maks+1
+         if(maks>maxOne):
+               maxOne=maks
+               basliklar=komb
+                     
+    b=""
+    for i in basliklar:
+          b=b+baslik[i].replace('\n','')
+    print("karsilastirilacak sutun sayisi : %s" %sutun)
+    print("%s : %s tane 1 var"%(b,maxOne))
+    print("")                
+        
